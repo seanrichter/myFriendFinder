8d9a8901b47ba6f068298b4fd9a2412a083cd293
Sean Richter <sean.richter04@outlook.com>
2019-10-21 21:56:46 -0500

    added folders and files, put server.js syntax and its console logging PORT 3000

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+//changed to true
+app.use(bodyParser.urlencoded({ extended: true }));
+//commented this out
+app.use(bodyParser.json());
+//app.use(express.static(__dirname + '/app/public')); 
+
+
+//from npm body-parser
+// parse application/json
+app.use(bodyParser.json());
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
