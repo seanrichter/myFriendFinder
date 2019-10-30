8bdf8d3edbcdfc2628b5452e882818e95640a92c
Sean Richter <sean.richter04@outlook.com>
2019-10-29 22:03:10 -0500

    heroku7

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ var PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 //changed to true
-app.use(express.urlencoded({ extended: true }));
-//commented this out
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/app/public/"));
 
 //require the html-routes
-require("./app/routing/api-route")(app);
-require("./app/routing/html-route")(app);
+require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes.js"))(app);
 // Could not get this to route the 
 
 
